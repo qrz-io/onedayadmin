628e09fd99403b682477de50caf33a03a0088f44
Cristian Quiroz <cdquirozc@gmail.com>
2015-03-04 23:27:21 +0000

    Added cron logic to deactivate admins

---
+++ app/code/community/Qrz/OneDayAdmin/Model/Cron.php
@@ -0,0 +1,40 @@
+<?php
+
+class Qrz_OneDayAdmin_Model_Cron
+{
+
+    /**
+     * @return void
+     * @author Cristian Quiroz <cris@qrz.io>
+     */
+    public function deactivateOneDayAdmins()
+    {
+        $oneDayAdminUserIdArray = $this->getAdminRolesModel()->load('One Day Admin', 'role_name')
+            ->getRoleUsers();
+
+        $oneDayAdminUsers = $this->getAdminUserModel()->getCollection()
+            ->addFieldToFilter('user_id', array('in' => $oneDayAdminUserIdArray));
+
+        foreach($oneDayAdminUsers as $oneDayAdminUser) {
+            $oneDayAdminUser->setData('is_active', 0)->save();
+        }
+    }
+
+    /**
+     * @return Mage_Admin_Model_Roles
+     * @author Cristian Quiroz <cris@qrz.io>
+     */
+    protected function getAdminRolesModel()
+    {
+        return Mage::getModel('admin/roles');
+    }
+
+    /**
+     * @return Mage_Admin_Model_User
+     * @author Cristian Quiroz <cris@qrz.io>
+     */
+    protected function getAdminUserModel()
+    {
+        return Mage::getModel('admin/user');
+    }
+}
