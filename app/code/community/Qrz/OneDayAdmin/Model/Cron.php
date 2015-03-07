66df5afdda2941ac41dd35c45db6ee148c1496f5
Cristian Quiroz <cdquirozc@gmail.com>
2015-03-07 17:26:49 +0000

    Updated cron to disable admins from all one day roles, as picked from the admin panel

--- app/code/community/Qrz/OneDayAdmin/Model/Cron.php
+++ app/code/community/Qrz/OneDayAdmin/Model/Cron.php
@@ -9,15 +9,25 @@ class Qrz_OneDayAdmin_Model_Cron
      */
     public function deactivateOneDayAdmins()
     {
-        $oneDayAdminUserIdArray = $this->getAdminRolesModel()
-            ->load(Qrz_OneDayAdmin_Helper_Data::ONE_DAY_ADMIN_ROLE_NAME, 'role_name')
-            ->getRoleUsers();
+        $oneDayAdminRoles = $this->getSystemConfig()->getOneDayAdminRoles();
 
-        $oneDayAdminUsers = $this->getAdminUserModel()->getCollection()
-            ->addFieldToFilter('user_id', array('in' => $oneDayAdminUserIdArray));
+        foreach ($oneDayAdminRoles as $oneDayAdminRole) {
+            $adminRole = $this->getAdminRolesModel()
+                ->load($oneDayAdminRole, 'role_name');
 
-        foreach($oneDayAdminUsers as $oneDayAdminUser) {
-            $oneDayAdminUser->setData('is_active', 0)->save();
+            if (!$adminRole->getId()) {
+                continue;
+            }
+
+            $adminUserIdArray = $adminRole->getRoleUsers();
+
+            $adminUserCollection = $this->getAdminUserModel()->getCollection()
+                ->addFieldToFilter('user_id', array('in' => $adminUserIdArray))
+                ->addFieldToFilter('is_active', 1);
+
+            foreach ($adminUserCollection as $adminUser) {
+                $adminUser->setData('is_active', 0)->save();
+            }
         }
     }
 
@@ -37,5 +47,14 @@ class Qrz_OneDayAdmin_Model_Cron
     protected function getAdminUserModel()
     {
         return Mage::getModel('admin/user');
+    }
+
+    /**
+     * @return Qrz_OneDayAdmin_Model_SystemConfig
+     * @author Cristian Quiroz <cris@qrz.io>
+     */
+    protected function getSystemConfig()
+    {
+        return Mage::getSingleton('qrz_onedayadmin/systemConfig');
     }
 }
